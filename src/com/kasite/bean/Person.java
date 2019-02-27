4b38cc919ca6fadd6c9bb70c9e88f835be72cebe
brothor <brothor@126.com>
2019-02-27 22:25:36 +0800

    test init

---
+++ src/com/kasite/bean/Person.java
@@ -0,0 +1,22 @@
+package com.kasite.bean;
+
+public class Person {
+	private Integer id;
+	private String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("javaproject");
+	}
+}
